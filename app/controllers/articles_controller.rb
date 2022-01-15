0ad30d93627770253aa1cd9f13b4039ab9c849b8
Sarah <dousse.sarah@gmail.com>
2022-01-15 17:43:54 +0000

    add form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new # instantiates new article but does not save it 
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params) # instantiates new article
+
+    if @article.save # save article
+      redirect_to @article # redirect_to : browser makes a new request, use after mutation database or app state
+    else
+      render :new, status: :unprocessable_entity  # renders the specified view fro the current request
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
