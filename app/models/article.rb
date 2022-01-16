b80087eb661ddb8465e0586269bc53ff82d57cf8
Sarah <dousse.sarah@gmail.com>
2022-01-16 21:48:03 +0000

    refactoring concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 } 
-
-  VALIDATES_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALIDATES_STATUSES }
-
-  def archived
-    status == 'archived'
-  end
 end
